5d0a3316be885b7bbebc5a327f3fba0b2a408dbf
Marcos Ceola <Marcos.VC321@gmail.com>
2021-09-12 00:18:13 -0300

    Adjust 2

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
     <>
+        <NavBar />
         <div className="container">
             <div className="jumbotron">
                 <h1 className="display-4">DSVendas</h1>
